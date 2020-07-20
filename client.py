847b3db8f141527b16371ff598085fc37c3e62d2
mohammadh223 <66203447+mohammadh223@users.noreply.github.com>
2020-07-20 15:51:38 +0430

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
-
-
 import socket
+
+counter=0
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host= socket.gethostname()
 ip = socket.gethostbyname(host)
@@ -8,10 +9,30 @@ server=(ip,8585)
 s.connect(server)
 connect=True
 while connect:
-    massage=s.recv(1024).decode()
-    if massage=='0':
-        connect=False
-    else:
-        print(massage)
-        message = input('Your Message : ')
-        s.send(message.encode())
+    H=s.recv(1024).decode()
+    if counter>0:
+        M=[0]*4
+        M=H
+        print(H) 
+        print(M[3])
+        if M[3]=='0':
+            s.send("0000".encode())
+            P=s.recv(1024).decode()
+            print(P)
+            connect=False 
+                  
+       
+    if connect:
+        A = input('please enter 3 number ')
+        A=int(A)
+        if counter<7:
+            massage=(A*10)+1
+        else :
+            massage=A*10  
+        massage=str(massage)      
+        s.send(massage.encode())
+        counter=counter+1
+        if counter==8:
+            M=s.recv(1024).decode()
+            print(M)
+            connect=False
